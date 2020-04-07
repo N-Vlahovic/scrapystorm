600768624d80410e8ca2e6ce46774a72968988bf
Niko <vlahovic.nikolai@gmail.com>
2020-04-07 12:53:27 +0200

    updated example

--- test/example.py
+++ test/example.py
@@ -1,10 +1,6 @@
 import os
-import pandas as pd
 from scrapystorm import API
 from typing import List, NoReturn
-
-
-task = API.get_task_by_name(task_name='INSTAGRAM_TASK_1')
 
 
 class Config:
@@ -14,16 +10,21 @@ class Config:
 def create_resource(task_id: int) -> NoReturn:
     """Create an excel file containing the URLs needed for our first task"""
     url_list: List[str] = ['https://www.instagram.com/sennheiser/', 'https://www.instagram.com/nexup_official/']
-    # df = pd.DataFrame(data=url_list, columns=['URL'])
     folder = f'{Config.resources_path}/{task_id}'
     if not os.path.isdir(folder):
         os.makedirs(folder)
     with open(f'{folder}/urls.txt', 'w') as file:
         file.write('\n'.join(url_list))
-    print(f'Saved at {folder}/urls.txt')
-    # df.to_excel(f'{folder}/urls.xlsx', index=False)
+
+
+def main(task_name: str):
+    task = API.get_task_by_name(task_name=task_name)
+    create_resource(task_id=task.task_id)
+    print(task.status())
+    print(f'Starting task\n{task}')
+    task.start()
+    print(task.status())
 
 
 if __name__ == '__main__':
-    create_resource(task_id=task.task_id)
-    print(task)
+    main(task_name='INSTAGRAM_TASK_1')
